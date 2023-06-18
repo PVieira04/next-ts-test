824003d89c36403300509bff5207ed9a496ceae8
PVieira04 <vieira.pjpv@gmail.com>
2023-06-18 20:23:19 +0000

    Add use of usePathname to deactivate login page link

--- frontend/src/app/NavBar.tsx
+++ frontend/src/app/NavBar.tsx
@@ -1,16 +1,20 @@
 "use client";
 
+import Link from 'next/link';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { usePathname } from 'next/navigation';
 
 export default function NavBar() {
+    const pathname = usePathname();
+
     return(
         <Navbar bg='primary' variant='dark' sticky='top' expand='sm' collapseOnSelect>
             <Container>
-                <Navbar.Brand href='/'>Home</Navbar.Brand>
+                <Navbar.Brand as={Link} href='/'>Home</Navbar.Brand>
                 <Navbar.Toggle aria-controls='main-navbar' />
                 <Navbar.Collapse id='main-navbar'>
                     <Nav>
-                        <Nav.Link href='/login'>Login</Nav.Link>
+                        <Nav.Link as={Link} href='/login' active={pathname === '/login'}>Login</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
