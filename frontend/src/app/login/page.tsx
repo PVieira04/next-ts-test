63b26cf29ee47693ba3d1bfb286122f2c52449e0
PVieira04 <vieira.pjpv@gmail.com>
2023-06-18 19:44:03 +0000

    Add error.tsx

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,4 +1,4 @@
 export default async function Login() {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     return <div>You can log in on this page.</div>
 }
