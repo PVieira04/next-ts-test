c3cfd292ba4f8b2f89666739bbd4d8a89e591746
PVieira04 <vieira.pjpv@gmail.com>
2023-06-18 20:14:54 +0000

    Add responsive hamburger menu to include login page in navbar

--- frontend/src/app/NavBar.tsx
+++ frontend/src/app/NavBar.tsx
@@ -1,12 +1,18 @@
 "use client";
 
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 export default function NavBar() {
     return(
         <Navbar bg='primary' variant='dark' sticky='top' expand='sm' collapseOnSelect>
             <Container>
                 <Navbar.Brand href='/'>Home</Navbar.Brand>
+                <Navbar.Toggle aria-controls='main-navbar' />
+                <Navbar.Collapse id='main-navbar'>
+                    <Nav>
+                        <Nav.Link href='/login'>Login</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
